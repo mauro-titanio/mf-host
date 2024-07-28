8d3ccf2cb3fa1587636c88dc70ef9d6b12fdd1c3
Mauro Titanio <maurotitanio@gmail.com>
2024-07-28 08:40:27 +0200

    Add remote url

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import federation from '@originjs/vite-plugin-federation'
-import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: 'app',
+      name: "app",
       remotes: {
-        remoteApp: 'http://localhost:5001/assets/remoteEntry.js',
+        remoteApp: "https://mf-remote-peach.vercel.app/assets/remoteEntry.js",
       },
-      shared: ['react','react-dom']
-    })
+      shared: ["react", "react-dom"],
+    }),
   ],
   build: {
     modulePreload: false,
-    target: 'esnext',
+    target: "esnext",
     minify: false,
-    cssCodeSplit: false
-  }
-})
+    cssCodeSplit: false,
+  },
+});
